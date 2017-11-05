ad6d9ea4b06d0a5e15bbcdfc0f4c6e33ed9ce8ff
arshadkhan35 <arshadkhan2631@gmail.com>
2017-11-05 12:21:13 +0530

    adding redirect ti thanku page after contact submit

--- modules/mcs_form_alter/src/Controller/UserContctSubmit.php
+++ modules/mcs_form_alter/src/Controller/UserContctSubmit.php
@@ -6,25 +6,24 @@ use \Drupal\node\Entity\Node;
 
 class UserContctSubmit extends ControllerBase{
 	public function save(){
-		kint(\Drupal::request()->request->all());
-		$build = array(
-      '#type' => 'markup',
-      '#markup' => t('Hello World!'),
-    );
-		// Create node object with attached file.
-$node = Node::create([
-  'type'        => 'connect',
-  'title'       => \Drupal::request()->request->get('email')	,
-  'field_full_name' => \Drupal::request()->request->get('username'),
-    'field_company_name' => \Drupal::request()->request->get('company'), 
-  'field_email_id' => \Drupal::request()->request->get('email'), 
-  'field_m' => \Drupal::request()->request->get('password'), 
- 
-]);
-$node->save();
-  return new \Symfony\Component\HttpFoundation\RedirectResponse(\Drupal::url('thanku'));
- // don't send the response yourself inside controller and form.
-//return;
+		//kint(\Drupal::request()->request->all());
+    		$build = array(
+          '#type' => 'markup',
+          '#markup' => t('Hello World!'),
+        );
+		// Create new node from contact-us forms.
+        $node = Node::create([
+          'type'        => 'connect',
+          'title'       => \Drupal::request()->request->get('email')	,
+          'field_full_name' => \Drupal::request()->request->get('username'),
+            'field_company_name' => \Drupal::request()->request->get('company'), 
+          'field_email_id' => \Drupal::request()->request->get('email'), 
+          'field_m' => \Drupal::request()->request->get('password'), 
+         
+        ]);
+        $node->save();
+        // Redirect to thank you page after save
+        return $this->redirect('mcs_form_alter.thanku');
   }
 	
 }
